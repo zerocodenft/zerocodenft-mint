7dc5c1d95ad063160d5b57cb3828dc7c32ee4a7e
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-06-27 12:26:27 -0500

    brave browser fix

--- mixins/headTag.js
+++ mixins/headTag.js
@@ -23,12 +23,21 @@ export default {
 			},
 		})
 
+		const isBraveBrowser = typeof navigator.brave !== 'undefined'
+		console.info({isBraveBrowser})
+
+		if(isBraveBrowser) {
+			// mock $gtag function since Brave blocks GA script
+			Vue.prototype.$gtag = function() {}
+		}
+
 		// analytics
 		scripts.push({
 			hid: 'gtag',
 			src: `https://www.googletagmanager.com/gtag/js?id=${this.$config.GTAG_ID}`,
 			async: true,
 			once: true,
+			skip: isBraveBrowser,
 			callback: () => {
 				window.dataLayer = window.dataLayer || []
 
