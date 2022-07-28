ba8413f08238c97f6ac1496f5bb0fec1e9475db9
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-07-27 22:21:39 -0500

    siteConfig plagin update

--- plugins/siteConfig.js
+++ plugins/siteConfig.js
@@ -1,38 +1,39 @@
 import siteConfigLocal from '@/siteConfig.json'
 
-export default async ({ redirect, route, $cloudFns, $axios }, inject) => {
+export default async ({ redirect, route, $cloudFns, $axios, app }, inject) => {
     
-    let siteConfig = {
-        ...siteConfigLocal,
-        configs: JSON.stringify(siteConfigLocal.configs),
-        stylesConfig: JSON.stringify(siteConfigLocal.stylesConfig)
-    }
+    let siteConfig
 
-    inject('siteConfig', siteConfig)
-    return
-
-    if(route.path === '/error') {
-        inject('siteConfig', siteConfig)
-        return
-    }
-    
-    const siteId = route.query['siteId'] || localStorage.getItem('siteId')
-    localStorage.setItem('siteId', siteId)
-    if(!siteId) {
-        redirect('/error?type=missingConfig')
-    }
+    if(app.context.isDev) {
+        siteConfig = {
+            ...siteConfigLocal,
+            configs: JSON.stringify(siteConfigLocal.configs),
+            stylesConfig: JSON.stringify(siteConfigLocal.stylesConfig)
+        }
+    } else {
+        if(route.path === '/error') {
+            inject('siteConfig', siteConfig)
+            return
+        }
         
-    try {
-        const { data } = await $cloudFns.get('/siteconfig', { params: { websiteId: siteId } })
-        siteConfig = data
-    }
-    catch(err1) {
-        try {
-            // fallback in case cloud function is not working
-            const { data } = await $axios.get(`/websites/${siteId}/config`)
-            siteConfig = data
-        } catch(err2) {
+        const siteId = route.query['siteId'] || localStorage.getItem('siteId')
+        localStorage.setItem('siteId', siteId)
+        if(!siteId) {
             redirect('/error?type=missingConfig')
+        }
+            
+        try {
+            const { data } = await $cloudFns.get('/siteconfig', { params: { websiteId: siteId } })
+            siteConfig = data
+        }
+        catch(err1) {
+            try {
+                // fallback in case cloud function is not working
+                const { data } = await $axios.get(`/websites/${siteId}/config`)
+                siteConfig = data
+            } catch(err2) {
+                redirect('/error?type=missingConfig')
+            }
         }
     }
 
